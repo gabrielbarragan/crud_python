c11e47bca55c6bf71efb7184664a361f0307791c
gabo <gabrielbarragan07@gmail.com>
2021-07-27 22:14:40 -0500

    CRUD con listas completado

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 clients = ['pablo', 'ricardo']
 
 def create_client (client_name):
@@ -16,6 +17,22 @@ def delete_client(client_name):
     else:
         clients.remove(client_name)
 
+def update_client(client_name):
+    global clients
+    
+    if client_name in clients:
+
+        old_client_index = clients.index(client_name)
+        update_client_name = input('What is the new client name?: ')
+
+        clients[old_client_index] = update_client_name
+        return update_client_name
+
+    else:
+        print("Client is not in client's list")
+        input('...')
+
+
 def list_clients():
     global clients
     cliente_str=', '
@@ -27,15 +44,27 @@ def _put_comma():
     clients+=','
 
 def _print_welcome():
+    global clients
     print('''
              *************************************************
              ||||||||||||| Welcome to Ventas CRUD ||||||||||||
-             *************************************************''')
+             *************************************************
+            ''')
+    print('This is de current list:', clients )
     print('What would you like to do today')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
     print('[Q]uit for Ventas CRUD')
+    
 
+def screen_clear():
+    # for mac and linux(here, os.name is 'posix')
+    if os.name == 'posix':
+        sistema = os.system('clear')
+    else:
+    # for windows platfrom
+        sistema = os.system('cls')
 
 if __name__ =='__main__':
 
@@ -44,18 +73,30 @@ if __name__ =='__main__':
     while paso == 'seguir':
 
         _print_welcome()
-        command = input('Please input option to do [C], [D] or [Q]: ').upper()
+        command = input('Please input option to do [C], [D], [U] or [Q]: ').upper()
+        
         if command == 'Q':
+            
             paso = 'parar'
-
-        elif command == 'C':
+        
+        elif command != 'Q':
+            
             client_name = input('What is the client name?: ')
-            create_client(client_name)
-            list_clients()
 
-        elif command == 'D':
-            client_name = input ('What is the client to delete?: ')
-            delete_client(client_name)
-            list_clients()
-        else: 
-            print('Invalid Command')
+            if command == 'C':
+                
+                create_client(client_name)
+                
+            elif command == 'D':
+
+                delete_client(client_name)
+
+            elif command == 'U':
+                
+                update_client(client_name)
+
+            else: 
+
+                print('Invalid Command')
+        
+        screen_clear()
